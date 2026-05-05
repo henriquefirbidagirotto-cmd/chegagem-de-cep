564689ca31a332a0fc8edcfc982bc3ed9c27aaef
henriquefirbidagirotto-cmd <henriquefirbidagirotto@gmail.com>
2026-05-05 20:56:00 -0300

    Add files via upload

---
+++ cep_model.py
@@ -0,0 +1,41 @@
+import requests
+from config import BASE_URL, FALLBACK_URL
+
+def buscar_cep_api_principal(cep):
+    try:
+        response = requests.get(
+        f'{BASE_URL}{cep}/json',
+        headers={'Acept':'application/json'},
+        timeout=5
+
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        
+        return None
+    except requests.exceptions.RequestException:
+        return None
+    
+def buscar_cep_api_fallback(cep):
+    try:
+        response = requests.get(
+        f'{FALLBACK_URL}{cep}',
+        headers={'Acept':'application/json'},
+        timeout=5
+
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        
+        return None
+    except requests.exceptions.RequestException:
+        return None
+    
+def buscar_cep(cep):
+    data = buscar_cep_api_principal(cep)
+    if data:
+        return data
+    
+    return buscar_cep_api_fallback(cep)
---
+++ cep_viewmodel.py
@@ -0,0 +1,19 @@
+from app.models.cep_model import buscar_cep
+
+def get_cep_data(cep):
+    if not cep or not cep. isdigit() or len (cep) != 8:
+        return {'erro':'cep invalido'}
+    
+    data = buscar_cep(cep)
+
+    if not data:
+        return {'erro': 'O CEP NAO FOI ENCONTRADO OU A API INVALIDA'}
+    
+    return{
+
+        'logradouro':data.get('logradouro'),
+        'bairro':data.get('bairro'),
+        'cidade':data.get('cidade') or data.get('localidade'),
+        'estado':data.get('uf') or data.get('estado')
+
+    }
